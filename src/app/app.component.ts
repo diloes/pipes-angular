a9cac1a4889f2937957492a204bdc1e5a7b65718
diloes <diloes@hotmail.com>
2023-07-21 16:04:54 +0200

    feat: add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
+
+/**
+ * NOTAS:
+ * - PUNTO DE ENTRADA APP: Este archivo es el punto de entrada a nuestra apliación.
+ */
